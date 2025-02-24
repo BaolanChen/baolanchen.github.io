9b721dbb6ece7f41f848889094fc6e193e5c0f61
BaolanChen <2711611445@qq.com>
2025-02-25 02:13:51 +0800

    Site updated: 2025-02-25 02:13:43

--- js/fomal.js
+++ js/fomal.js
@@ -3093,7 +3093,7 @@ if (localStorage.getItem("blogbg") != undefined) {
   setBg(localStorage.getItem("blogbg"));
 } else {
   document.getElementById("defineBg").innerText = `:root{
-    --default-bg: url(https://s21.ax1x.com/2025/02/25/pE1tqTP.jpg);
+    --default-bg: url(https://source.fomal.cc/img/home_bg.webp);
     --darkmode-bg:url(https://s21.ax1x.com/2025/02/25/pE1tTOA.jpg);
     --mobileday-bg: url(https://s21.ax1x.com/2025/02/25/pE1tbwt.jpg);
     --mobilenight-bg: url(https://s21.ax1x.com/2025/02/25/pE1t5Se.jpg);
